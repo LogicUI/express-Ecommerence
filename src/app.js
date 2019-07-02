140bb1144d7f9e6c0c24657bacccf2fb7426fb4b
john <johnwee35@gmail.com>
2019-07-02 21:09:11 +0800

    added process port for heroku

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 require('./db');
 const gamesRoute = require('./routes/games');
 const consoleRoute = require('./routes/consoles');
